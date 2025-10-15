287026c71922b4ef7c6f118fb45a4c475f843e2e
pradeeprayudu016 <rayudupradeep10@gmail.com>
2025-10-15 09:52:47 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,50 @@
+/*#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+}*/
+/*#include<stdio.h>
+int main()
+{
+	int n,m;
+	scanf("%d %d",&n,&m);
+	int a[n][m];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			printf("%d ",a[j][i]);
+		}
+		printf("\n");
+	}
+}*/
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n][n];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			scanf("%d")
+		}
+	}
+}
